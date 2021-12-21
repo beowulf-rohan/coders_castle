ad50852834f84d61858783dbdd15abf1270a32c1
Ladi Rahul <rahulladi8686@gmail.com>
2021-12-21 23:28:44 +0530

    internet page handeled

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,31 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:async';
 import 'package:coders_castle/screens/homePage.dart';
-import 'package:coders_castle/widgets/resuable.dart';
+import 'intermediateScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:coders_castle/screens/homeScreen.dart';
 import 'package:coders_castle/screens/profileScreen/profileScreen.dart';
 import 'package:coders_castle/screens/contestScreen/contestScreen.dart';
 import 'package:coders_castle/screens/statsScreen/statsScreen.dart';
-import 'package:coders_castle/modals/contests_list.dart';
+import 'package:provider/provider.dart';
+import 'package:coders_castle/check_internet_connectivity/connectivity_provider.dart';
+
+import 'check_internet_connectivity/no_internet.dart';
+import 'modals/contests_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-      routes: {
-        HomeScreen.id: (context) => HomeScreen(),
-        ProfileScreen.id: (context) => ProfileScreen(),
-        ContestScreen.id: (context) => ContestScreen(),
-        StatsScreen.id: (context) => StatsScreen(),
-        HomePage.id: (context) => HomePage(),
-      },
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  double widget1Opacity = 0.0;
+class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     get();
-    Timer(Duration(seconds: 5), () {
-      Navigator.pushReplacementNamed(context, HomePage.id);
-    });
-    // Timer(Duration(milliseconds: 500), () {
-    //   Navigator.pushNamed(context, HomeScreen.id);
-    // });
-    Future.delayed(const Duration(milliseconds: 1000), () {
-      setState(() {
-        widget1Opacity = 1;
-      });
-    });
     super.initState();
   }
 
@@ -68,35 +38,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: GlassBackground(
-          childWidget: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedOpacity(
-                  opacity: widget1Opacity,
-                  duration: Duration(milliseconds: 2500),
-                  child: SizedBox(
-                    height: 150,
-                    width: 150,
-                    child: Image.asset('images/logo.png'),
-                  ),
-                ),
-                SizedBox(height: 50.0),
-                Text(
-                  'CODERS CASTLE',
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ConnectivityProvider(),
+          child: InterMediateScreen(),
         ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: InterMediateScreen(),
       ),
     );
   }
